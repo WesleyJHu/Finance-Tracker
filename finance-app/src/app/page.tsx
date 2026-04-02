0f5ec4f23738898ac1801b6a02c34ebe78551cc2
weswes2EPYC <whu327@gatech.edu>
2026-04-01 21:54:28 -0400

    tested Accounts API routes

--- finance-app/src/app/page.tsx
+++ finance-app/src/app/page.tsx
@@ -2,14 +2,16 @@
 
 import React from 'react';
 import Card from '../components/AccountCard';
-import TestApiButton from '@/components/TestApiButton';
+import TestTransactionApiButton from '@/components/TestTransactionApiButton';
+import TestAccountApiButton from '@/components/TestAccountApiButton';
 
 export default function AboutPage() {
   return (
     <main className="p-8">
       <h1 className="text-3xl font-bold mb-4">Hi</h1>
       <Card name="Fidelity Credit Card" limit={6500} value={4200} />
-      <TestApiButton />
+      <TestTransactionApiButton />
+      <TestAccountApiButton />
     </main>
   );
 }
